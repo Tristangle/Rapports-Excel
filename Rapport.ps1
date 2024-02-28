d08608852f7d9794d701dd9d9f189dd5b9480ee6
Tristan BERDRIN <tberdrin@myges.fr>
2024-02-28 11:13:51 +0100

    Export de données excel valides

--- Rapport.ps1
+++ Rapport.ps1
@@ -1,24 +1,48 @@
 #Objectif » Récupérer les noms et champs de tables dans un Excel, puis générer un nouvel Excel avec un Script PowerShell
 
 # Acceder à un dossier
-$emplacement = Set-Location -Path "F:\08-DAFI\06-Informatique\01. Projet IFS\_technique\database.export"
+$emplacement = Set-Location -Path "C:\Users\tb50919\Documents\databaseExport"
 # Lister les fichiers présents
-$liste = Get-ChildItem($emplacement)
-Write-Output($liste)
+$dossier = Get-ChildItem($emplacement)
+
+#Write-Output($liste)
+
 # Créer le fichier Excel de rapport
-$FichierRapport = New-Object -ComObject Excel.Application
-$FichierRapport.Visible = $True
+$excel = New-Object -ComObject Excel.Application
+$excel.Visible = $True
+$excel.DisplayAlerts = $False
+
 # Ouvre une page dans le fichier de Rapport
-$Rapport = $FichierRapport.Workbooks.add()
+$Rapport = $excel.Workbooks.add()
 $FeuilleRapport = $Rapport.worksheets.Item(1)
 # Boucle sur la liste des Excels présents dans le dossier
-
-    # Ouvrir le fichier
+$emplacementNom = 1
+$emplacementChamps = 3
+foreach($fichier in $dossier){
+    # Ouvrir le fichier existant
+    $nomFichier = $fichier.FullName
+    $fichierExcel = $excel.Workbooks.Open($nomFichier)
 
     # Copier le nom du fichier, puis le coller dans le Rapport
+    $FeuilleRapport.Cells.Item(1, $emplacementNom) = $fichier.Name
+    # Accéder à la feuille de calcul du fichier Excel ouvert
+    $feuilleExcel = $fichierExcel.Worksheets.Item(1)
+    # Boucle sur la liste des champs de la table
+    foreach ($cellule in $feuilleExcel.UsedRange.Columns) {
+        $champ = $cellule.Cells.Item(1, 1).Value2
+        # Copier les champs de table puis les coller dans l'Excel de rapport
+        $FeuilleRapport.Cells.Item($emplacementChamps, $emplacementNom) = $champ
+        $emplacementChamps++
+    }
+    $emplacementChamps = 3
+    $emplacementNom = $emplacementNom +=2
 
-        # Boucle sur la liste des champs de la table
 
-            # Copier les champs de table puis les coller dans l'Excel
-
+    $fichierExcel.Close()
+}
+#Redimensionner la cellule pour avoir un résultat lisible 
+$FeuilleRapport.Columns.AutoFit()
 # Enregistre le fichier Excel dans le dossier
+#$Rapport.SaveAs($emplacement)
+#$Rapport.Close()
+#$excel.Quit()
